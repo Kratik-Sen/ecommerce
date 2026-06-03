3a38d7584ed53ddb40c9bbecee3e153f810f1780
kratik <kratiksen854@gmail.com>
2026-06-03 14:11:52 +0530

    add backend url

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:3000"
+    let serverUrl = "https://ecommerce-backend-6r3d.onrender.com"
 
     let value = {
        serverUrl
